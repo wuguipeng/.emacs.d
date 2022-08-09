9963d15b3eb8966d6ec3058849744722146d5640
Wu-Guipeng <xyxdbp2022@gmail.com>
2022-08-10 00:20:38 +0800

    delete elpa

--- elpa/keycast-20220422.1611/keycast-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from keycast.el  -*- no-byte-compile: t -*-
-(define-package "keycast" "20220422.1611" "Show current command and its binding" '((emacs "25.3") (compat "28.1.1.0")) :commit "296fba536287e7a0d88109e75a0bc0181647dc5e" :authors '(("Jonas Bernoulli" . "jonas@bernoul.li")) :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :keywords '("multimedia") :url "https://github.com/tarsius/keycast")
